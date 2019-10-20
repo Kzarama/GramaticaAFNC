f83a271b3236c73f12a4c115d41cf66274498258
kz <kevin.zarama1@correo.icesi.edu.co>
2019-10-20 18:55:46 -0500

    gui 'finished'

---
+++ src/controllerFNC/ControllerFNC.java
@@ -0,0 +1,15 @@
+package controllerFNC;
+
+public class ControllerFNC {
+
+	public void accept() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void reboot() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/viewFNC/GramaticFNC.java
+++ src/viewFNC/GramaticFNC.java
@@ -7,42 +7,66 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import com.sun.xml.internal.messaging.saaj.soap.JpegDataContentHandler;
+
+import controllerFNC.ControllerFNC;
+
 public class GramaticFNC extends JFrame implements ActionListener {
+	
+	private ControllerFNC controller;
+	
+	JPanel aux = new JPanel(new BorderLayout());
 	
 	public GramaticFNC() {
 		int numProductions  = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el numero de producciones", "Gramatica a FNC", JOptionPane.INFORMATION_MESSAGE));
 		createView(numProductions);
-		
+		add(aux);
 		setSize(500, 500);
-		setLayout(new BorderLayout());
 		setVisible(true);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-//		pack();
+		pack();
 	}
 	
 	private void createView(int numProductions) {
-		JTextField[] productions = new JTextField[numProductions];
-		JPanel panelProductions = new JPanel(new GridLayout(numProductions + 1, 1));
+		JTextField[][] productions = new JTextField[numProductions][2];
+		JPanel panelProductions = new JPanel(new GridLayout(numProductions, 3));
 		for (int i = 0; i < numProductions; i++) {
-			productions[i] = new JTextField();
-			panelProductions.add(productions[i]);
+			productions[i][0] = new JTextField();			
+			panelProductions.add(productions[i][0]);
+			JLabel arrow = new JLabel("-->");
+			panelProductions.add(arrow);
+			productions[i][1] = new JTextField();
+			panelProductions.add(productions[i][1]);
 		}
-		JButton button = new JButton("acept");
-//		add(panelProductions, BorderLayout.CENTER);
-//		add(button, BorderLayout.SOUTH);
-		add(panelProductions);
-//		add(button);
+		JLabel labMessage = new JLabel("Ingrese aqui las producciones");
+		JButton buttonAccept = new JButton("aceptar");
+		buttonAccept.setActionCommand("ACCEPT");
+		buttonAccept.addActionListener(this);
+		JButton buttonReboot = new JButton("Reiniciar");
+		buttonReboot.setActionCommand("REBOOT");
+		buttonReboot.addActionListener(this);
+		JPanel panelButtons = new JPanel(new GridLayout(1,2));
+		panelButtons.add(buttonAccept);
+		panelButtons.add(buttonReboot);
+		aux.add(labMessage, BorderLayout.NORTH);
+		aux.add(panelProductions, BorderLayout.CENTER);
+		aux.add(panelButtons, BorderLayout.SOUTH);
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+		String command = e.getActionCommand();
+		if(command.equals("ACCEPT")) {
+			controller.accept();
+		} else {
+			controller.reboot();
+		}
 	}
 	
 	public static void main(String[] args) {
