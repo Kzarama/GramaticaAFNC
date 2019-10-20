f5d53e954533b15625139572db9dbe5b0fb048bb
kz <kevin.zarama1@correo.icesi.edu.co>
2019-10-20 16:25:07 -0500

    .

--- src/viewFNC/GramaticFNC.java
+++ src/viewFNC/GramaticFNC.java
@@ -1,17 +1,42 @@
 package viewFNC;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class GramaticFNC extends JFrame implements ActionListener {
 	
 	public GramaticFNC() {
+		int numProductions  = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el numero de producciones", "Gramatica a FNC", JOptionPane.INFORMATION_MESSAGE));
+		createView(numProductions);
 		
+		setSize(500, 500);
+		setLayout(new BorderLayout());
 		setVisible(true);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+//		pack();
+	}
+	
+	private void createView(int numProductions) {
+		JTextField[] productions = new JTextField[numProductions];
+		JPanel panelProductions = new JPanel(new GridLayout(numProductions + 1, 1));
+		for (int i = 0; i < numProductions; i++) {
+			productions[i] = new JTextField();
+			panelProductions.add(productions[i]);
+		}
+		JButton button = new JButton("acept");
+//		add(panelProductions, BorderLayout.CENTER);
+//		add(button, BorderLayout.SOUTH);
+		add(panelProductions);
+//		add(button);
 	}
 	
 	@Override
